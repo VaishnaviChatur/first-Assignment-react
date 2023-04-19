b75513b963b02c5b5dff956517b2a04373a1a7b3
vaishnavichatur <vaishnavichatur5@gmail.com>
2023-04-20 02:08:39 +0530

    part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
+import logo from './logo.svg';
 import './App.css';
-import { Button } from './components/Button';
-import {ReactBenifits} from './components/ReactBenifits';
-import { ReactLogo } from "./components/ReactLogo";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <ReactLogo />
-        <ReactBenifits/>
-        <Button/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <list>
+        <ol>
+          <li> Component-based architecture</li>
+    <li>  Virtual DOM for efficient Updates</li> 
+     <li>Rick ecosystem and community</li> 
+     <li>Cross-platform development</li>
+   <li>  Strong community support:</li>
+        </ol>
+      </list>
+        <button>Get Started ...</button>
+        
       </header>
-      
     </div>
-    
   );
+ 
+  
 }
 
 export default App;
+
+
+
